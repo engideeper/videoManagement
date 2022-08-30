80286447a805adb9e8c8a856149c89325f34be07
Austen <austen.guo@tcsoft.net.cn>
2022-08-30 17:21:51 +0800

    视频同部

---
+++ src/components/video.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: Austen austen.guo@tcsoft.net.cn
+ * @Date: 2022-08-30 15:09:17
+ * @LastEditors: Austen austen.guo@tcsoft.net.cn
+ * @LastEditTime: 2022-08-30 15:09:57
+ * @FilePath: \video-management\src\components\video.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <video-player
+      src="/your-path/video.mp4"
+      poster="/your-path/poster.jpg"
+      controls
+      :loop="true"
+      :volume="0.6"
+    />
+  </template>
+  
+  <script>
+    import { defineComponent } from 'vue'
+    import { VideoPlayer } from '@videojs-player/vue'
+    import 'video.js/dist/video-js.css'
+  
+    export default defineComponent({
+      components: {
+        VideoPlayer
+      }
+    })
+  </script>
--- src/pages/Card/index.vue
+++ src/pages/Card/index.vue
@@ -1,28 +1,48 @@
 <template>
   <div>
+    <div class="flex justify-end">
+
+    <div class="flex-none  h-16 ...">
+      
+      <el-upload
+    class="upload-demo"
+    action="@/src/assets/videos/t1.mp4"
+    :on-preview="handlePreview"
+    :on-remove="handleRemove"
+    :before-remove="beforeRemove"
+    :limit="3"
+    :on-exceed="handleExceed"
+  >
+    <el-button type="primary">视频上传</el-button>
+  </el-upload>
+  </div>
+  <div class="flex-none h-16 ...">
+    <el-input
+      class="w-50 m-2"
+      size="large"
+      placeholder="Please Input"
+      :prefix-icon="Search"
+    />
+  </div>
+    </div>
     <div class="card-container">
-      <div
-        class="item-wrapper pt-6"
-        @click="dialogVisible = true"
-        v-for="(item, index) in list"
-        :key="item"
-      >
+      <div class="item-wrapper pt-6" @click="dialogVisible = true" v-for="(item, index) in list" :key="item">
         <div :class="getClass(index)">
-          video thumbnail 
+         视频缩略图
           <!-- {{ item+1 }} -->
         </div>
         <div class=" flex-col text-left text_show text-xs">
           <div class="flex leading-tight text-xs justify-between">
-            <span class="font-bold">错误代码 DMU0002412</span>   
-            <el-button type="success" size="small" >编辑</el-button>
-          </div>        
-          <div class="flex leading-tight truncate ... text-xs justify-between" >
-            <span class="font-bold">标签 DMU|仪器|故障|重置|</span> 
-           <el-button id="del_btn" size="small" type="danger" >删除 </el-button>
+            <span class="font-bold">错误代码 DMU0002412</span>
+            <el-button type="success" size="small">编辑</el-button>
+          </div>
+          <div class="flex leading-tight truncate ... text-xs justify-between">
+            <span class="font-bold">标签 DMU|仪器|故障|重置|</span>
+            <el-button id="del_btn" size="small" type="danger">删除 </el-button>
           </div>
           <div class="flex leading-tight text-xs justify-between">
-             <span class="font-bold">上传时间 2022-08-22</span>   
-            <el-button type="success" size="small" >详情</el-button>
+            <span class="font-bold">上传时间 2022-08-22</span>
+            <el-button type="success" size="small">详情</el-button>
           </div>
         </div>
       </div>
@@ -38,146 +58,159 @@
 </template>
 
 <script setup>
-import { reactive, ref, onMounted } from "vue";
-import { useCounterStore } from "@/stores/counter";
-const src =
-  "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg";
-const list = reactive([]);
-let dialogVisible = ref(false);
 
-onMounted(() => {
-  for (let i = 0; i < 15; i++) {
-    list.push(i);
-  }
-});
+  import {
+    reactive,
+    ref,
+    onMounted
+  } from "vue";
+  import {
+    useCounterStore
+  } from "@/stores/counter";
 
-const counter = useCounterStore();
+  import { Search } from '@element-plus/icons-vue'
+  import { ElMessage, ElMessageBox } from 'element-plus'
 
-const handleAdd = () => {
-  // counter.count++;
-  counter.increment();
-};
+// import type { UploadProps, UploadUserFile } from 'element-plus'
+  const src =
+    "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg";
+  const list = reactive([]);
+  let dialogVisible = ref(false);
 
-const handleMin = () => {
-  counter.count--;
-};
+  onMounted(() => {
+    for (let i = 0; i < 15; i++) {
+      list.push(i);
+    }
+  });
 
-const getClass = (index) => {
-  const classes = ["card-item"];
-  const key = Math.random();
-  if (key < 0.1) {
-    classes.push("item-0");
-  } else if (key < 0.25) {
-    classes.push("item-1");
-  } else if (key < 0.45) {
-    classes.push("item-2");
-  } else if (key < 0.65) {
-    classes.push("item-3");
-  } else if (key < 0.85) {
-    classes.push("item-4");
-  } else if (key < 1) {
-    classes.push("item-5");
-  }
-  return classes;
-};
+  const counter = useCounterStore();
 
-const handleOpenDialog = () => {
-  dialogVisible = true;
-};
+  const handleAdd = () => {
+    // counter.count++;
+    counter.increment();
+  };
+
+  const handleMin = () => {
+    counter.count--;
+  };
+
+  const getClass = (index) => {
+    const classes = ["card-item"];
+    const key = Math.random();
+    if (key < 0.1) {
+      classes.push("item-0");
+    } else if (key < 0.25) {
+      classes.push("item-1");
+    } else if (key < 0.45) {
+      classes.push("item-2");
+    } else if (key < 0.65) {
+      classes.push("item-3");
+    } else if (key < 0.85) {
+      classes.push("item-4");
+    } else if (key < 1) {
+      classes.push("item-5");
+    }
+    return classes;
+  };
+
+  const handleOpenDialog = () => {
+    dialogVisible = true;
+  };
 </script>
 
 <style scoped>
-.card-container {
-  width: 100%;
-  background-color: #fff;
-  justify-content: space-around;
-  display: grid;
-  /* grid-gap: 20px 20px; */
-  grid-template-columns: repeat(auto-fill, 300px);
-}
+  .card-container {
+    width: 100%;
+    background-color: #fff;
+    justify-content: space-around;
+    display: grid;
+    /* grid-gap: 20px 20px; */
+    grid-template-columns: repeat(auto-fill, 300px);
+  }
 
-.item-wrapper {
-  height: 260px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction:column;
-}
+  .item-wrapper {
+    height: 260px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.text_show{
-  width:260px;
-}
-.card-item {
-  border-radius: 8px;
-  width: 260px;
-  height: 180px;
-  position: relative;
-  text-align: center;
-  line-height: 150px;
-  transition: all 0.1s linear;
-  --tw-gradient-to: #06b6d4;
-  --tw-gradient-from: #4ade80;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(74 222 128/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .text_show {
+    width: 260px;
+  }
 
-.card-item.item-0 {
-  --tw-gradient-to: #db2777;
-  --tw-gradient-from: #fb923c;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(251 146 60/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item {
+    border-radius: 8px;
+    width: 260px;
+    height: 180px;
+    position: relative;
+    text-align: center;
+    line-height: 150px;
+    transition: all 0.1s linear;
+    --tw-gradient-to: #06b6d4;
+    --tw-gradient-from: #4ade80;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(74 222 128/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-.card-item.item-1 {
-  --tw-gradient-to: #6366f1;
-  --tw-gradient-from: #a855f7;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(168 85 247/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item.item-0 {
+    --tw-gradient-to: #db2777;
+    --tw-gradient-from: #fb923c;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(251 146 60/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-.card-item.item-2 {
-  --tw-gradient-to: #0ea5e9;
-  --tw-gradient-from: #22d3ee;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(34 211 238/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item.item-1 {
+    --tw-gradient-to: #6366f1;
+    --tw-gradient-from: #a855f7;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(168 85 247/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-.card-item.item-3 {
-  --tw-gradient-to: #06b6d4;
-  --tw-gradient-from: #4ade80;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(74 222 128/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item.item-2 {
+    --tw-gradient-to: #0ea5e9;
+    --tw-gradient-from: #22d3ee;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(34 211 238/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-.card-item.item-4 {
-  --tw-gradient-to: #2563eb;
-  --tw-gradient-from: #38bdf8;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(56 189 248/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item.item-3 {
+    --tw-gradient-to: #06b6d4;
+    --tw-gradient-from: #4ade80;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(74 222 128/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-.card-item.item-5 {
-  --tw-gradient-to: #f43f5e;
-  --tw-gradient-from: #ec4899;
-  --tw-gradient-stops: var(--tw-gradient-from),
-    var(--tw-gradient-to, rgb(236 72 153/0));
-  background-image: linear-gradient(to right, var(--tw-gradient-stops));
-}
+  .card-item.item-4 {
+    --tw-gradient-to: #2563eb;
+    --tw-gradient-from: #38bdf8;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(56 189 248/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
 
-/* .card-item:hover {
+  .card-item.item-5 {
+    --tw-gradient-to: #f43f5e;
+    --tw-gradient-from: #ec4899;
+    --tw-gradient-stops: var(--tw-gradient-from),
+      var(--tw-gradient-to, rgb(236 72 153/0));
+    background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  }
+
+  /* .card-item:hover {
   height: 200px;
   width: 300px;
 } */
 </style>
 
 <style>
-.card-dialog .el-dialog__header {
-  padding: 0;
-}
+  .card-dialog .el-dialog__header {
+    padding: 0;
+  }
 </style>
